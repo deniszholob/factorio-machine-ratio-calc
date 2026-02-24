bba60be96ade6da5119e4da823ef550efe81eaca
Denis Zholob <dddgamer3x@gmail.com>
2026-02-24 11:15:15 -0700

    fix tooltip not showing

--- src/app/components/generic/tooltip/tooltip.component.ts
+++ src/app/components/generic/tooltip/tooltip.component.ts
@@ -3,7 +3,7 @@ import { Component, input } from '@angular/core';
 @Component({
   selector: 'app-tooltip',
   templateUrl: './tooltip.component.html',
-  host: { class: 'contents' },
+  host: { class: 'block' }, // Directive applies positioning here, cannot be "contents" unless directive logic is changed
   imports: [],
 })
 export class TooltipComponent {
